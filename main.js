14c24ef8734b07555dd263f5aa1232a8219574ff
nebalmaz16 <nebalmaz16@gmail.com>
2018-10-20 13:52:19 -0400

    added pop ups windows

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 let age;
 console.log("age is currently;" + age);
 age=26;
-
+console.log("age is currently;" + age)
+console.log "hello world"
 age=26 + 1;
+let username = prompt("what is your name?");
+let var1 = confirm("just checking , is " + username + "your real name?");
+if(nameconfirm) {
+    SpeechRecognitionAlternative("well come to my page, " + username + "!");
+}
+else {
+    username = prompt("okay, what is your name?");
+}
+
+console.log("the user's name is " + username + " . ")
+}
+}
